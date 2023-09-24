2962b891b64ad922a5aecea07d55040c601d088b
lilrachel1985 <lilrachel1985@gmail.com>
2023-09-24 17:33:06 +0530

    Add files via upload
    This explores the concept of F-Strings

---
+++ time_to_live.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+new_age=int(age)
+new_age1=90-new_age
+#Write your code below this line 👇
+
+print(f"You have {new_age1*365} days, {new_age1*52} weeks, and {new_age1*12} months left.")
+
+
