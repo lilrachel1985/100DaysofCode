1126571164357db1106db088f3b15181d6e5e289
lilrachel1985 <lilrachel1985@gmail.com>
2023-09-26 15:43:29 +0530

    Treasure_island.py

---
+++ main (1).py
@@ -0,0 +1,47 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+game_start = input('Your at a crossroad.Do u want to go left or right')
+level1 = game_start.lower()
+if level1 == 'right':
+    print('You fell into a hole.Game Over')
+elif level1 == 'left':
+    level2 = input('Do u want to wait for a boat or swim')
+    level2.lower()
+    if level2 == 'wait':
+        level3 = input(
+            'which door do you want to enter by? Choose Red Blue Yellow')
+        level3.lower()
+        if level3 == 'red':
+            print('Burned By Fire. Game Over')
+        elif level3 == 'blue':
+            print('Eaten by Beasts.Game Over')
+        else:
+            print('Yay. You have crossed all levels and won the game')
+    elif level2 == 'swim':
+        print('Attacked by crocodiles. Game Over')
