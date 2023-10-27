50f7c7826cd9f3d97c1293a105ea2904ff8281e5
lilrachel1985 <lilrachel1985@gmail.com>
2023-10-27 10:24:10 +0530

    Add files via upload

---
+++ multiplication_table.py
@@ -0,0 +1,14 @@
+def mult(n):
+  i=0
+  #n=int(input("enter the number:"))
+  while i in range(0,11):
+     result=n*i
+     print(f" {n} * {i} = {result}")
+     i=i+1
+     
+mult(9)
+    
+
+
+
+
---
+++ sum_of_digits.py
@@ -0,0 +1,5 @@
+string=input("Enter a number:")
+sum=0
+for c in map(int, string):
+  sum=sum+c
+print(sum)
