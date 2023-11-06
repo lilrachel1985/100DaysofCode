dbb124984947f3055f1ae06d88dfe8a926145615
lilrachel1985 <lilrachel1985@gmail.com>
2023-11-06 11:27:24 +0530

    Add files via upload

---
+++ average_dict.py
@@ -0,0 +1,5 @@
+total = [{"date": "2014-03-01", "value": 200}, {"date": "2014-03-02", "value": 100},{"date": "2014-03-03", "value": 400}]
+print sum(d['value'])
+print average(float(sum/len(total)))
+
+    
